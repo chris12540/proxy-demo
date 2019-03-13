ca607236023422bd42db102a98303709c1668a38
Tyler Collier <tyler@intraedge.com>
2019-03-12 22:27:33 -0700

    Add Yelp

---
+++ README.md
@@ -0,0 +1,14 @@
+# Proxy Demo
+
+
+## Proxy
+
+Run a proxy on `http://127.0.0.1:8888`. For example, [Charles Proxy]().
+
+Using an http agent, such as your browser or [Postman](https://www.getpostman.com/), visit [http://localhost:4000/data][http://localhost:4000/data] and note that the information was captured by the proxy.
+
+## Yelp
+
+Save `.example.env` as `.env`. Get an API key from the Yelp Developers portal. Fill out `YELP_APIKEY`.
+
+Using an http agent, such as your browser or [Postman](https://www.getpostman.com/), visit [http://localhost:4000/yelp][http://localhost:4000/yelp] and note that the information was captured by the proxy.
--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 const express = require('express')
 const axios = require('axios')
-
-
+require('dotenv').config()
+const yelp = require('yelp-fusion')
+const ProxyAgent = require('https-proxy-agent')
 
 
 
 // YOUR AXIOS CALLS WILL NOT WORK IF YOU DON'T HAVE A PROXY RUNNING HERE.
+// Unless you comment this out.
 axios.defaults.proxy = {
   host: '127.0.0.1',
   port: 8888
 }
-
-
-
-
 
 const app = express()
 app.use(express.json())
@@ -24,6 +22,30 @@ app.get('/data', (req, res) => {
   axios.get('http://jsonplaceholder.typicode.com/users/' + id)
     .then(response => {
       res.json(response.data)
+    })
+    .catch(error => {
+      console.error(error)
+      res.status(500).json({
+        message: 'An unexpected error occurred on the server'
+      })
+    })
+})
+
+// YOUR YELP CALLS WILL NOT WORK UNLESS YOU HAVE A PROXY RUNNING.
+// Since it uses SSL, we use process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0.
+// Do not do `process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0` in a production environment,
+// it's just for debugging.
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
+app.get('/yelp', (req, res) => {
+  const yelpClient = yelp.client(process.env.YELP_APIKEY, {
+    agent: new ProxyAgent('http://127.0.0.1:8888')
+  })
+  yelpClient.search({
+    term:'Four Barrel Coffee',
+    location: 'san francisco, ca'
+  })
+    .then(response => {
+      res.json(response.jsonBody)
     })
     .catch(error => {
       console.error(error)
